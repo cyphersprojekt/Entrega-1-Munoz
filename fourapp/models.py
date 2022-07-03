38cedb91dc7031a11cd6d60e956057819c9139f2
cyphersprojekt <munoz.ignacioagustin@gmail.com>
2022-07-03 18:48:17 -0300

    agrego dos modelos tontos para probar que todo este bien

--- fourapp/models.py
+++ fourapp/models.py
@@ -1,3 +1,19 @@
+from email.mime import image
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    categoryid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    postid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/', blank=True)
+    def __str__(self):
+        return self.title
