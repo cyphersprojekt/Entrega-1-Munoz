e49682aaf3f5ab43fc27aceb2a9d87db8c3a004f
cyphersprojekt <munoz.ignacioagustin@gmail.com>
2022-07-03 20:16:34 -0300

    borro al carajo todo lo que hice porque de todas formas no funcionaba

--- fourapp/views.py
+++ fourapp/views.py
@@ -7,13 +7,3 @@ from .forms import PostForm
 
 def index(request):
     return render(request, 'index.html')
-
-def crear_post(request):    
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return render(request, 'index.html')
-    else:
-        form = PostForm()
-    return render(request, 'crear_post.html', {'form': form})
