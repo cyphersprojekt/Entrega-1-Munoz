0d1b58ef438a2530c60c77fc24e8e8c2788e7f8e
cyphersprojekt <munoz.ignacioagustin@gmail.com>
2022-07-04 21:15:35 -0300

    estuve dos horas para que la verga esta permita subir un post sin imagen. la solucion no es nada elegante

--- fourapp/views.py
+++ fourapp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import redirect, render
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from .models import Category, Post
 from .forms import PostForm
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'categories': Category.objects.all()})
 
 def image(request, image_id):
     image = Post.objects.get(post_id=image_id)
@@ -16,11 +17,19 @@ def new_post(request):
         username = request.POST['username']
         title = request.POST['title']
         content = request.POST['content']
-        image = request.FILES['image']
         category = request.POST['category']
+        # Por algún motivo que no comprendo, si la imagen está vacía
+        # la poronga esta retorna un diccionario raro en lugar de NADA.
+        # así que si eso sucede, piso el valor de 'image' para que no me rompa
+        # las pelotas
+        try:
+            image = request.FILES['image']
+        except:
+            image = None
         post = Post(username=username, title=title, content=content, image=image, category=Category.objects.get(categoryid=category))
+        print(post)
         post.save()
-        return redirect('index')
+        return redirect('/')
 
 def view_category(request, category_id):
     category = Category.objects.get(categoryid=category_id)
